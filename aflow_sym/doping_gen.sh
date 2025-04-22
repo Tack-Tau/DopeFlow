745eab95121ead3e54f9727974819bb9f13ef21c
Tack_Tau <taoshuo950111@gmail.com>
2025-04-22 10:46:15 -0400

    Clean-up

--- aflow_sym/doping_gen.sh
+++ aflow_sym/doping_gen.sh
@@ -8,12 +8,15 @@
 #SBATCH --mail-user=EMAIL_PLACEHOLDER
 
 module purge
-module use /projects/community/modulefiles
-module load intel/17.0.4 python/3.8.5-gc563
+# module use /projects/community/modulefiles
+# module load intel/17.0.4 python/3.8.5-gc563
 # export OMP_NUM_THREADS=1
 
 ulimit -s unlimited
 ulimit -s
 
-rm uniq_poscar_list doping_log 2> /dev/null
-echo -e "Si\nGe\n7\n200" | python3 SiGe_doping.py > doping_log
+source ~/.bashrc
+conda activate shape
+
+rm -r test_* uniq_poscar_list doping_log 2> /dev/null
+echo -e "Si\nGe\n5\n500\ny" | python3 fp_cluster_doping.py > doping_log
